19fefd28e96e09c4ceb536029ffc036045dc8d63
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-09-29 18:24:34 -0500

    Cors Update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(`${__dirname}/data/db.json`);
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-const port = process.env.PORT || 3000;
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
 })
-
 server.use(router);
 server.use(middlewares)
 
